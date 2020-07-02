aca65afb045cbfbe2ddb20d4b4e06d697d0c52f9
bishalroy <broy@stats.com>
2020-07-02 21:00:00 +0530

    Book Class Added

---
+++ BusinessRulesEngine/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine
+{
+    public class Book : PhysicalProduct
+    {
+        public Book() : base()
+        {
+            
+        }
+    }
+}
