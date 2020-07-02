f47fd5c60e83d9382d87ef024278dff7c1c67005
bishalroy <broy@stats.com>
2020-07-02 21:37:01 +0530

    Adding MembershipDetails method inMmebership class to get email status.

--- BusinessRulesEngine/Membership.cs
+++ BusinessRulesEngine/Membership.cs
@@ -16,6 +16,24 @@ namespace BusinessRulesEngine
             _emailID = emailID;
             _membershipType = productType;
             Console.WriteLine("We are processing your Request");
+            MembershipDetails(_membershipType, _emailID);
+        }
+
+        //To Call Send Email Method and check the status of email triggered.
+        public void MembershipDetails(string productType, string emailID)
+        {
+            Email email = new Email();
+            bool emailStatus;
+            emailStatus = email.SendEmail(emailID, productType, "Membership is activated/upgraded.");
+            if (emailStatus)
+            {
+                Console.WriteLine("Confirmation Email Sent Successfully");
+            }
+
+            else
+            {
+                Console.WriteLine("Our team will manually sent you a Confirmation Mail");
+            }
         }
     }
 }
