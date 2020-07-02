d9712c426ca6473dbbfc54ca9d2bcc58fc7c1816
bishalroy <broy@stats.com>
2020-07-02 21:54:56 +0530

    Removing junk code

--- BusinessRulesEngine/Book.cs
+++ BusinessRulesEngine/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessRulesEngine
+﻿namespace BusinessRulesEngine
 {
     public class Book : PhysicalProduct
     {
