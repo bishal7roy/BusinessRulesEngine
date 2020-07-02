7c699a2263a53192dcf8800a47211c4215b5c8ed
bishalroy <broy@stats.com>
2020-07-02 21:45:15 +0530

    Calling FirstAid from Video Class.

--- BusinessRulesEngine/Video.cs
+++ BusinessRulesEngine/Video.cs
@@ -13,7 +13,7 @@ namespace BusinessRulesEngine
             Console.WriteLine("Packing Slip Generated");
             if (videoType.ToLower().Equals("video (learning to ski)"))
             {
-               
+                new FirstAid();   //For specific video First aid video will be provided free.
             }
         }
     }
