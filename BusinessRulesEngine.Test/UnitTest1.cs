737a299fe87978e1bda54ea3ed553d11dd772a43
bishalroy <broy@stats.com>
2020-07-02 20:55:58 +0530

    Creating Application, Adding TDD and ProductDeatils Class

---
+++ BusinessRulesEngine.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRulesEngine.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InputUserValue_ReturnsValue()
+        {
+            ProductDetails productDetails = new ProductDetails();
+
+            var result = productDetails.InputUserValue(1);
+
+            Assert.AreEqual(result, 1);
+
+        }
+    }
+}
