2676f48f7c20df3f303f62c708f3c76302318b58
bishalroy <broy@stats.com>
2020-07-02 21:40:55 +0530

    Adding Video Class

---
+++ BusinessRulesEngine/Video.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine
+{
+    class Video
+    {
+        public Video(string videoType)
+        {
+            Console.WriteLine("Packing Slip Generated");
+            if (videoType.ToLower().Equals("video (learning to ski)"))
+            {
+               
+            }
+        }
+    }
+}
