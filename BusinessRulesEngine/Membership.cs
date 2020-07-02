0209e6e032a1a23bd71eeeca00c7d763a05904c9
bishalroy <broy@stats.com>
2020-07-02 21:17:54 +0530

    Adding Mebership Class

---
+++ BusinessRulesEngine/Membership.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine
+{
+    public class Membership
+    {
+        private string _membershipType = "";
+        private string _emailID = "";
+
+        public Membership(string productType, string emailID)
+        {
+            _emailID = emailID;
+            _membershipType = productType;
+            Console.WriteLine("We are processing your Request");
+        }
+    }
+}
